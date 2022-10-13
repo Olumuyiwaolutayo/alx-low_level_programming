f1ca8edfaf28b2aaee1f823389559cea6a643d91
Olumuyiwaolutayo <olumuyiwaolutayo@yahoo.com>
2022-10-13 13:01:45 -0700

    ammend

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  *main -is the entry point for the program.
  *
- *Return:zero if no error, Non-zero value  if errors.
+ *Return:zero if no error, Non-zero value  if errors. 
  */
 int main(void)
 {
