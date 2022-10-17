02353f1c8885c83da7f4f64c02a8230e612160ee
Olumuyiwaolutayo <olumuyiwaolutayo@yahoo.com>
2022-10-17 07:36:03 -0700

    2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,7 +24,8 @@ int main(void)
 			putchar('0' + j / 10);
 			putchar ('0'+ j % 10);
 
-			if (i == 98 && j == 99)				break;
+			if (i == 98 && j == 99)
+				break;
 			putchar(',');
 			putchar(' ');
 		}
