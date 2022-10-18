56d06088416d8a99eb66308c738a31afec79db6b
Olumuyiwaolutayo <olumuyiwaolutayo@yahoo.com>
2022-10-18 07:04:24 -0700

    main.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include <main.h>
 
 /**
- * print_alphabeth - print all alphabeth in lowercase
+ * print_alphabet - print all alphabet in lowercase
  */
-void print_alphabeth(void)
+void print_alphabet(void)
 {
 	char latter;
 
